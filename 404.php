78c1adeb6fe04569f1f710def76b94b2880b0619
Castor <whoisyoges@castorisdead.xyz>
2023-01-09 12:21:09 +0545

    Added error page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://castorisdead.xyz/assets/images/avatar/avatar.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://castorisdead.xyz/assets/images/avatar/avatar.png">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Error - Page not found</title>
+</head>
+
+<body>
+    <div class="guestbookarea">
+        <h3>Error</h3>
+        <p>The requested page could not be found. Please try again later.</p>
+        <p>If you think this is an issue, Please <a href="https://castorisdead.xyz/contact/" target="_blank">contact me</a>. </p>
+    </div>
+</body>
+
+</html>
